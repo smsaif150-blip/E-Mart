924f14f5c4256bcc8dfa6ad5eb1c9782c7f064c7
SHAWAL MAHMUD SAIF <smsaif150@gmail.com>
2026-07-04 11:28:15 +0600

    Login and Register fragment design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
